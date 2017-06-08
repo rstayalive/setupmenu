f8755030b157be1747cf46df2675a9d09b28db1d
rstayalive <rstayalive@gmail.com>
2017-06-08 17:12:04 +0300

    added double if check for perl-text setup

--- src/xtables_geoip.sh
+++ src/xtables_geoip.sh
@@ -31,12 +31,13 @@ else
 	br
 fi
 }
-workdir='/root/setupmenu/src'
 
-#Устанавливаем geoip
+workdir='/root/setupmenu/src'
+arc=`arch`
+
+#Начало установки
 clear
 	echo "Начинаем установку"
-		br
 			echo "Ставим зависимости"
 				myinstall gcc 
 				myinstall gcc-c++ 
@@ -48,11 +49,39 @@ clear
 				myinstall kernel-devel-$(uname -r) 
 				myinstall iptables-devel
 				myinstall epel-release
-				rpm -i ftp://rpmfind.net/linux/dag/redhat/el6/en/x86_64/dag/RPMS/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
-				rpm -i $workdir/perl-Text-CSV_XS-0.80-1.el6.rf.x86_64.rpm
+				
+#Весь ниже написанный головняк связан исключительно с пакетом perl-Text-CSV_XS
+
+#Проверяем разрядность для установки репозитория
+		if [ "$arc" == "x86_64" ];
+		then rpm -i ftp://rpmfind.net/linux/dag/redhat/el6/en/x86_64/dag/RPMS/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
+		else rpm -i ftp://rpmfind.net/linux/dag/redhat/el6/en/i386/dag/RPMS/rpmforge-release-0.5.3-1.el6.rf.i686.rpm
+		fi
+		echo "Установлен репозиторий $arc"	
 		
+#Пробуем ставить из репозитория
+		myinstall perl-Text-CSV_XS
+		
+#На всякий случай проверяем, поставился perl-Text-CSV_XS из репозитория или нет, если не поставился, то ставим из rpm SRC директории скрипта.		
+		PKG_OK=$(rpm -qa | grep perl-Text-CSV_XS)
+		echo Checking for somelib: $PKG_OK
+		if [ "" == "$PKG_OK" ];  #проверяем поставился perl-Text-CSV_XS из репозитория или нет
+		then
+		echo "Не найден в репозитории, ставим из rpm"
+		if [ "$arc" == "x86_64" ]; #проверяем разрядность, чтобы не гадать и не спрашивать какую версию ставить.
+		then
+		rpm -i $workdir/perl-Text-CSV_XS-0.80-1.el6.rf.x86_64.rpm
+		echo "установлена x64 версия"
+		else
+		rpm -i $workdir/perl-Text-CSV_XS-0.85-1.el6.i686.rpm
+		echo "установлена x86 версия"
+		fi
+		else
+		echo "Уже установлено, пропускаем"
+		fi
+		
+#Если все ок, то ставим сам уже geoip модуль.
 		echo "ставим geoip"
-	br
 		cp $workdir/xtables-addons-1.47.1.tar.xz /tmp
 		cd /tmp
 		tar xvf xtables-addons-1.47.1.tar.xz
@@ -61,10 +90,8 @@ clear
 		cd /lib/modules/$(uname -r)/build/include/linux/
 		replace "#define CONFIG_IP6_NF_IPTABLES_MODULE 1" "/*#define CONFIG_IP6_NF_IPTABLES_MODULE 1*/" -- autoconf.h
 		cd /tmp/xtables-addons-1.47.1
-	br
 		echo "Запускаю компиляцию модуля"
 		make && make install
-	br
 		sleep 5
 		cd geoip/ 
 		./xt_geoip_dl 
