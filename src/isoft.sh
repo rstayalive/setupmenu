7b60ec5694a3132ce3610aefa78003dd5955130b
rstayalive <rstayalive@gmail.com>
2017-06-19 09:03:32 +0300

    error fix of sngrep installation

--- src/isoft.sh
+++ src/isoft.sh
@@ -59,7 +59,5 @@ sleep 3
 make
 speep 3
 make install
-alias sngrep='NCURSES_NO_UTF8_ACS=1 sngrep'
-echo export NCURSES_NO_UTF8_ACS=1 >> /etc/environment
 echo "Установлено, можешь теперь жать кнопки"
 waitend
