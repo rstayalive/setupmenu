f0aac613439855b2fd62b22231f0d5d8754a24bd
rstayalive <rstayalive@gmail.com>
2017-07-13 12:26:57 +0300

    cosmetic cleanup

--- src/missedreport.sh
+++ src/missedreport.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 #Скрипт настройки крона на уведомление о пропущенных звонках на email
-
 #Алиасы
 RED=\\e[91m
 GRE=\\e[92m
 DEF=\\e[0m
-
 #wait
 wait()
 {
 echo -e "$GRE Нажмите любую клавишу $DEF"
 read -s -n 1
 }
-
-#Кастом wait
-waitend()
+#end
+end()
 {
 echo -e "$GRE Нажмите любую клавишу чтобы вернуться в меню $DEF"
 read -s -n 1
 }
-
 #Y/N
 myread_yn()
 {
@@ -33,7 +29,6 @@ done
 eval $1=$temp
 }
 workdir='/root/setupmenu/src'
-
 #Начало работы
 clear
 echo "Начинаю настройку"
@@ -64,4 +59,4 @@ echo -e "$GREСделать тестовый запуск? (Y)/(N)$DEF"
 		n|N)
 		echo "Все настроено и готово к работе!" ;;
 esac
-waitend
+end
