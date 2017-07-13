0cf3af4977981b2562e589e288c45980ec83f37a
rstayalive <rstayalive@gmail.com>
2017-07-13 12:58:43 +0300

    cosmetic fixes

--- src/checksslsertificate.sh
+++ src/checksslsertificate.sh
@@ -4,8 +4,8 @@
 RED=\\e[91m
 GRE=\\e[92m
 DEF=\\e[0m
-#wait
-wait()
+#end
+end()
 {
 echo -e "$GRE Для продолжения нажмите любую клавишу $DEF"
 read -s -n 1
@@ -22,7 +22,7 @@ echo "Проверяю Ваш сертификат"
 curl --insecure -v https://$domain:10150 2>&1 | awk 'BEGIN { cert=0 } /^\* SSL connection/ { cert=1 } /^\*/ { if (cert) print }' >> /tmp/certlog.txt
 echo -e "$REDДата истечения сертификата$DEF"
 grep 'expire date' /tmp/certlog.txt
-rm -rvf /tmp/cert*
-echo -e "$GREВсе готово, ключи были обновлены!$DEF"
+rm -rf /tmp/cert*
+mail -s testemail $email < /dev/null
 fi
-wait
+end
