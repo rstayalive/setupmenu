04578aa7a212f421eb20e14cfca637b9d89a7eae
rstayalive <rstayalive@gmail.com>
2018-02-08 12:20:16 +0300

    error fix

--- src/checksslsertificate.sh
+++ src/checksslsertificate.sh
@@ -10,18 +10,17 @@ end()
 echo -e "$GREДля продолжения нажмите любую клавишу $DEF"
 read -s -n 1
 }
+startdate=$(curl --insecure -v https://127.0.0.1:10150 2>&1 | grep 'start date')
+expiredate=$(curl --insecure -v https://127.0.0.1:10150 2>&1 | grep 'expire date')
 domain=`hostname`
-#Проверяем настроен доменный сертификат или нет (тупо чекаем hostname, т.к при получении сертификата через certificate management нужно задавать hostname согласно сделанной A записи на домене)
+#Проверяем настроен доменный сертификат или нет (тупо чекаем hostname, т.к при получении сертификата через certificate management нужно задавать hostname согласно сделанной A записи в dns домена)
 #соответственно если hostname localhost.localdomain ничего настроено не было и смысла выполнять скрипт нет.
 if [ "$domain" == "localhost.localdomain" ];
 then
 echo -e "$REDДоменный сертификат не настроен или не изменен hostname, продолжить невозможно! Получите сначала сертификат и измените hostname! выходим...$DEF"
 else
 clear
-curl --insecure -v https://127.0.0.1:10150 2>&1 | grep "start date" && curl --insecure -v https://127.0.0.1:10150 2>&1 | grep "expire date" >> /tmp/certlog.txt
-echo -e "$REDДата истечения сертификата$DEF"
-grep 'expire date' /tmp/certlog.txt
-sleep 2
-rm -rf /tmp/cert*
+echo -e "Дата начала действия сертификата$startdate
+Дата окончания действия сертификата$expiredate"
 fi
 end
