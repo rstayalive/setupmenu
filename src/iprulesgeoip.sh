33eea916d7f19ca65192944d344b77caeba8ee3d
rstayalive <rstayalive@gmail.com>
2018-04-12 14:38:37 +0300

    removed user agents.

--- src/iprulesgeoip.sh
+++ src/iprulesgeoip.sh
@@ -44,20 +44,7 @@ echo -e "$GREПравила успешно добавлены в iptables и п�
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
 iptables -A INPUT -m state --state INVALID -j DROP
-iptables -I INPUT -p udp --dport 5060 -m string --string "friendly-scanner" --algo bm -j DROP
-iptables -I INPUT -p udp --dport 5060 -m string --string "sip-scan" --algo bm -j DROP
-iptables -I INPUT -p udp --dport 5060 -m string --string "sundayddr" --algo bm -j DROP
-iptables -I INPUT -p udp --dport 5060 -m string --string "iWar" --algo bm -j DROP
-iptables -I INPUT -p udp --dport 5060 -m string --string "sipsak" --algo bm -j DROP
-iptables -I INPUT -p udp --dport 5060 -m string --string "sipvicious" --algo bm -j DROP
-iptables -I INPUT -p udp --dport 5060 -m string --string "sipcli" --algo bm -j DROP
-iptables -I INPUT -p udp --dport 5060 -m string --string "eyeBeam" --algo bm -j DROP
-iptables -I INPUT -p udp --dport 5060 -m string --string "VaxSIPUserAgent" --algo bm -j DROP
-iptables -I INPUT -p udp --dport 5060 -m string --string "sip:nm@nm" --algo bm -j DROP
-iptables -I INPUT -p udp --dport 5060 -m string --string "sip:carol@chicago.com" --algo bm -j DROP
-iptables -I INPUT -p udp --dport 5060 -m string --string "FPBX" --algo bm -j DROP
-iptables -I INPUT -p udp --dport 5060 -m string --string "Z 3.14.38765 rv2.8.3" --algo bm -j DROP
-iptables -I INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A INPUT -p tcp --tcp-flags ALL ACK,RST,SYN,FIN -j DROP
 iptables -A INPUT -p tcp --tcp-flags SYN,FIN SYN,FIN -j DROP
 iptables -A INPUT -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
