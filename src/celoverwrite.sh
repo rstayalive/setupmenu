837b7b51d84bdb1834b49552a18b2f0f08f497d5
rstayalive <rstayalive@gmail.com>
2018-05-04 11:54:10 +0300

    new version.

--- src/celoverwrite.sh
+++ src/celoverwrite.sh
@@ -1,22 +1,12 @@
 #! /bin/bash
-# Скрипт замены файла cel.conf для работы простых звонков.
-# Часто после обновы модулей, переписывается этот файл.
-#Алиасы
-RED=\\e[91m
-GRE=\\e[92m
-DEF=\\e[0m
+# Скрипт вносит необходимые изменения в cel_general_custom для работы простых звонков
 
-#wait
-wait()
-{
-echo -e "$GRE Нажмите любую клавишу $DEF"
-read -s -n 1
-}
+
 
 #Кастом wait
-waitend()
+end()
 {
-echo -e "$GRE Нажмите любую клавишу чтобы вернуться в меню $DEF"
+echo -e "Нажмите любую клавишу чтобы вернуться в меню"
 read -s -n 1
 }
 
@@ -24,14 +14,23 @@ workdir='/root/setupmenu/src'
 
 #Начало работы
 clear
-	echo "Провожу замену cel.conf"
-	cp /var/www/html/admin/modules/cel/etc/cel.conf /root/cel.conf_orig
-	rm -f /var/www/html/admin/modules/cel/etc/cel.conf
-	cp $workdir/cel.conf /var/www/html/admin/modules/cel/etc/
-	fwconsole chown
-	echo "Готово, перезапускаю астериск"
-	fwconsole reload
-waitend
+#	echo "Провожу замену cel.conf"
+#	cp /var/www/html/admin/modules/cel/etc/cel.conf /root/cel.conf_orig
+#	rm -f /var/www/html/admin/modules/cel/etc/cel.conf
+#	cp $workdir/cel.conf /var/www/html/admin/modules/cel/etc/
+#	fwconsole chown
+#	echo "Готово, перезапускаю астериск"
+#	fwconsole reload
+
+echo "[general]
+enable=yes
+apps=dial,park,mixmonitor
+events=ALL
+
+[manager]
+enabled=yes" >> /etc/asterisk/cel_general_custom.conf
+
+end
 
 	
 	
