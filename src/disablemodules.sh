2b941987d6369fe6b13f19065b48b7055e8eea0c
rstayalive <rstayalive@gmail.com>
2018-06-25 15:57:12 +0300

    modules list recheck

--- src/disablemodules.sh
+++ src/disablemodules.sh
@@ -19,41 +19,53 @@ read -s -n 1
 
 #Начало работы
  echo "Отключаю модули"
-    fwconsole ma disable bria
+    fwconsole ma disable areminder
     fwconsole ma disable broadcast
-    fwconsole ma disable bulkdids
-    fwconsole ma disable bulkextensions
     fwconsole ma disable callerid
     fwconsole ma disable calllimit
     fwconsole ma disable conferencespro
+    fwconsole ma disable configedit
+    fwconsole ma disable contactmanager
+    fwconsole ma disable cos
+    fwconsole ma disable customappsreg
     fwconsole ma disable cxpanel
     fwconsole ma disable dahdiconfig
+    fwconsole ma disable daynight
+    fwconsole ma disable dictate
     fwconsole ma disable digium_phones
+    fwconsole ma disable digiumaddoninstaller
+    fwconsole ma disable directory
+    fwconsole ma disable disa
+    fwconsole ma disable dundicheck
     fwconsole ma disable endpoint
+    fwconsole ma disable fax
     fwconsole ma disable faxpro
     fwconsole ma disable freepbx_ha
+    fwconsole ma disable hotelwakeup
+    fwconsole ma disable irc
+    fwconsole ma disable motif
     fwconsole ma disable pagingpro
     fwconsole ma disable parkpro
+    fwconsole ma disable pbdirectory
     fwconsole ma disable pinsetspro
+    fwconsole ma disable queuestats
     fwconsole ma disable recording_report
     fwconsole ma disable restapps
     fwconsole ma disable sangomacrm
     fwconsole ma disable sipstation
-    fwconsole ma disable ucpnode
+    fwconsole ma disable sms
+    fwconsole ma disable tts
+    fwconsole ma disable ttsengines
     fwconsole ma disable vmnotify
     fwconsole ma disable voicemail_report
     fwconsole ma disable vqplus
     fwconsole ma disable webcallback
+    fwconsole ma disable webrtc
     fwconsole ma disable xmpp
     fwconsole ma disable zulu
-    fwconsole ma disable irc
-    fwconsole ma disable sng_mcu
-    fwconsole ma disable ucpnode
-    fwconsole ma disable sangomacrm
-    fwconsole ma disable rmsadmin
-    fwconsole ma disable vmnotify
-    fwconsole ma disable cos
-    fwconsole ma disable reminder
+    fwconsole ma disable pms
+    fwconsole ma disable qxact_reports
+    fwconsole ma disable vega
  echo "Модули успешно отключены"
     fwconsole reload
 waitend
