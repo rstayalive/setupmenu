66e87ddcf1434d18fd2e21b9331a4db8957712b8
rstayalive <rstayalive@gmail.com>
2019-05-31 17:03:47 +0300

    Create cdrfix.sh
    cdr codepage fix

---
+++ src/cdrfix.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#cdr codepage fix
+
+odbc=rpm -qa | grep mysql-connector-odbc
+rpm -e --nodeps $odbc
+yum install mariadb-connector-odbc
+
+if grep -F -x '[MariaDB]' /etc/odbcinst.ini;
+then
+echo "уже настроено"
+else
+echo "[MariaDB]
+Description=ODBC for MariaDB
+Driver=/usr/lib64/libmaodbc.so
+Setup=/usr/lib64/libodbcmyS.so
+UsageCount=2" >> /etc/odbcinst.ini
+fi
+fwconsole restart
+clear
+echo "Готово!"
