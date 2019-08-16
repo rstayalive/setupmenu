9d2277700e13016cf710ca807544e20b18af1892
rstayalive <rstayalive@gmail.com>
2019-08-16 11:49:25 +0300

    bug fix

--- src/izab.sh
+++ src/izab.sh
@@ -7,14 +7,14 @@ system=$(grep -oE '[0-9]+\.[0-9]+' /etc/redhat-release)
 		then
             rpm -Uvh http://repo.zabbix.com/zabbix/4.2/rhel/6/x86_64/zabbix-agent-4.2.5-1.el6.x86_64.rpm
                 replace "Server=127.0.0.1" "Server=176.192.230.26" -- /etc/zabbix/zabbix_agentd.conf
-                    replace "ServerActive=127.0.0.1" "Server=176.192.230.26" -- /etc/zabbix/zabbix_agentd.conf
+                    replace "ServerActive=127.0.0.1" "ServerActive=176.192.230.26" -- /etc/zabbix/zabbix_agentd.conf
                         replace "Hostname=Zabbix server" "Hostname=$hname" -- /etc/zabbix/zabbix_agentd.conf
                             echo "Установлен zabbix-agent под $system"
                                 service zabbix-agent restart
         else
             rpm -Uvh http://repo.zabbix.com/zabbix/4.2/rhel/7/x86_64/zabbix-agent-4.2.5-1.el7.x86_64.rpm
                 replace "Server=127.0.0.1" "Server=176.192.230.26" -- /etc/zabbix/zabbix_agentd.conf
-                    replace "ServerActive=127.0.0.1" "Server=176.192.230.26" -- /etc/zabbix/zabbix_agentd.conf
+                    replace "ServerActive=127.0.0.1" "ServerActive=176.192.230.26" -- /etc/zabbix/zabbix_agentd.conf
                         replace "Hostname=Zabbix server" "Hostname=$hname" -- /etc/zabbix/zabbix_agentd.conf
                             echo "Установлен zabbix-agent под $system"
                                 systemctl restart zabbix-agent
