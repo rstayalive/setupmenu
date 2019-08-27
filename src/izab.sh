6afb3baaad7ab4d98a905c9493316f084cdb3794
rstayalive <rstayalive@gmail.com>
2019-08-27 10:39:47 +0300

    some fix

--- src/izab.sh
+++ src/izab.sh
@@ -10,22 +10,24 @@ system=$(grep -oE '[0-9]+\.[0-9]+' /etc/redhat-release)
 		then
             rpm -Uvh http://repo.zabbix.com/zabbix/4.2/rhel/6/x86_64/zabbix-agent-4.2.5-1.el6.x86_64.rpm
                 if [ "$Pak" == "zabbix-agent" ]
-                    then echo "Установлен zabbix-agent под $system"
-                        else echo "Пакет не установился! Запускаю устанвоку второго варианта скрипта"
+                    then 
+                        echo "Установлен zabbix-agent под $system"
+                    else 
+                        echo "Пакет не установился! Запускаю устанвоку второго варианта скрипта"
                         bash /root/setupmenu/src/izab2.sh
-                        fi
-                            replace "Server=127.0.0.1" "Server=176.192.230.26" -- /etc/zabbix/zabbix_agentd.conf
-                                replace "ServerActive=127.0.0.1" "ServerActive=176.192.230.26" -- /etc/zabbix/zabbix_agentd.conf
-                                    replace "Hostname=Zabbix server" "Hostname=$hname" -- /etc/zabbix/zabbix_agentd.conf                    
-                                        replace "# ListenPort=10050" "ListenPort=$port" -- /etc/zabbix/zabbix_agentd.conf
-                                            service zabbix-agent restart
-                                            chkconfig zabbix-agent on
-                                            if [ "$irule" == "$port" ]
-                                                then echo "правило уже есть"
-                                                else 
-                                                    iptables -A INPUT -p tcp --dport $port -m state --state NEW,ESTABLISHED -j ACCEPT
-                                                        service iptables save
-                                            fi
+                fi
+                    replace "Server=127.0.0.1" "Server=176.192.230.26" -- /etc/zabbix/zabbix_agentd.conf
+                        replace "ServerActive=127.0.0.1" "ServerActive=176.192.230.26" -- /etc/zabbix/zabbix_agentd.conf
+                            replace "Hostname=Zabbix server" "Hostname=$hname" -- /etc/zabbix/zabbix_agentd.conf                    
+                                replace "# ListenPort=10050" "ListenPort=$port" -- /etc/zabbix/zabbix_agentd.conf
+                                    service zabbix-agent restart
+                                    chkconfig zabbix-agent on
+                                    if [ "$irule" == "$port" ]
+                                        then echo "правило уже есть"
+                                        else 
+                                            iptables -A INPUT -p tcp --dport $port -m state --state NEW,ESTABLISHED -j ACCEPT
+                                                service iptables save
+                                    fi
         else
             rpm -Uvh http://repo.zabbix.com/zabbix/4.2/rhel/7/x86_64/zabbix-agent-4.2.5-1.el7.x86_64.rpm
               if [ "$Pak" == "zabbix-agent" ]
