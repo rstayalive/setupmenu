ea23e2d4b50bd13f1af021595dab31b48bc24f85
rstayalive <rstayalive@gmail.com>
2020-02-17 14:31:02 +0300

    Update vedisoft_r.sh

--- src/vedisoft_r.sh
+++ src/vedisoft_r.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
-#удаление модуля простых звонков
-#end
+#Vedisoft module removing script
 end()
 {
-echo -e "Нажмите любую клавишу чтобы вернуться в меню"
+echo -e "Press any key to continue"
 read -s -n 1
 }
-
-echo "Начинаю удалять модуль простых звонков"
+myread_yn()
+{
+temp=""
+while [[ "$temp" != "y" && "$temp" != "Y" && "$temp" != "n" && "$temp" != "N" ]]
+do
+echo -n "y/n: "
+read -n 1 temp
+echo
+done
+eval $1=$temp
+}
+echo -e "\nYou relealy want to delete vedisoft module?"
+myread_yn ans
+case "$ans" in
+y|Y) 
+echo "Deleting vedisoft module"
 {
 fwconsole ma uninstall prostiezvonki
 fwconsole ma delete prostiezvonki
@@ -19,5 +32,8 @@ rm -rvf /usr/lib64/asterisk/modules/cel_prostiezvonki.so
 rm -rvf /tmp/prostiezvonki*
 unlink /var/www/html/records
 } &> /dev/null
-echo "Модуль успешно удален!"
+echo "Module vedisoft successfuly deleted!" ;;
+n|N)
+echo "Skipping" ;;
+esac
 end
