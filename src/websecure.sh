292de34c318397158d991c7240b445a806480e7d
rstayalive <rstayalive@gmail.com>
2020-02-21 14:33:35 +0300

    Update websecure.sh

--- src/websecure.sh
+++ src/websecure.sh
@@ -1,23 +1,34 @@
 #! /bin/bash
-# Скрипт вешает авторизацию на вебморду через .htaccess
-
-#Алиасы
-RED=\\e[91m
-GRE=\\e[92m
-DEF=\\e[0m
-
-#Кастом wait
-waitend()
+#This script configuring httpd to secure asterisk web interface .htaccess
+end()
 {
-echo -e "$GRE Нажмите любую клавишу чтобы вернуться в меню! $DEF"
+echo -e "Press any key to continue"
 read -s -n 1
 }
-
-echo "Настройка."
+myread_yn()
+{
+temp=""
+while [[ "$temp" != "y" && "$temp" != "Y" && "$temp" != "n" && "$temp" != "N" ]]
+do
+echo -n "y/n: "
+read -n 1 temp
+echo
+done
+eval $1=$temp
+}
+if [ -s "/home/asterisk/.htpasswd" ];
+then
+echo -e "\nAlready configured, do you want reconfigure?"
+myread_yn ans
+case "$ans" in
+y|Y) 
+rm -rf /home/asterisk/.htpasswd
+rm -rf /var/www/html/.htaccess
+echo "Job started"
 touch /var/www/html/.htaccess
 echo '
 AuthType Basic
-AuthName "fuck closed"
+AuthName "Closed"
 AuthUserFile /home/asterisk/.htpasswd
 SetEnvIf Request_URI "^/records/*" allow
 Order allow,deny
@@ -25,17 +36,41 @@ Require valid-user
 Allow from env=allow
 Deny from env=!allow
 Satisfy any
-
-
 <Files .htpasswd>
 deny from all
 </Files>' > /var/www/html/.htaccess 
-
-
-echo -e "\n$GREВведите пароль на доступ к веб интерфейсу$DEF"
+echo -e "\nEnter password to secure"
 read pass ;
 htpasswd -c -b /home/asterisk/.htpasswd sadmin $pass
 htpasswd -b /home/asterisk/.htpasswd records records
 service httpd restart
-echo -e "$GREНастройка завершена!$DEF"
-waitend
+echo -e "Job done, admin login: sadmin."
+echo -e "\nLogin for records: records and Passwd: records" ;;
+n|N)
+echo -e "Skipping" ;;
+esac
+else
+echo "Job started"
+touch /var/www/html/.htaccess
+echo '
+AuthType Basic
+AuthName "Closed"
+AuthUserFile /home/asterisk/.htpasswd
+SetEnvIf Request_URI "^/records/*" allow
+Order allow,deny
+Require valid-user
+Allow from env=allow
+Deny from env=!allow
+Satisfy any
+<Files .htpasswd>
+deny from all
+</Files>' > /var/www/html/.htaccess 
+echo -e "\nEnter password to secure"
+read pass ;
+htpasswd -c -b /home/asterisk/.htpasswd sadmin $pass
+htpasswd -b /home/asterisk/.htpasswd records records
+service httpd restart
+echo -e "Job done, admin login: sadmin."
+echo -e "\nLogin for records: records and Passwd: records"
+fi
+end
