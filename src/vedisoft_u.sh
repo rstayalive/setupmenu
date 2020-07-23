ce092c42e22a98a99b84e03e250bc0aef0ded2e2
rstayalive <rstayalive@users.noreply.github.com>
2020-07-23 11:07:44 +0300

    Update vedisoft_u.sh

--- src/vedisoft_u.sh
+++ src/vedisoft_u.sh
@@ -9,6 +9,13 @@ mkbackup()
 {
 bash /root/setupmenu/src/vedisoft_b.sh
 }
+cinfo()
+{
+grep password /etc/asterisk/cel_prostiezvonki.conf >> /backup_vedisoft/conf.txt
+grep record_external_path /etc/asterisk/cel_prostiezvonki.conf >> /backup_vedisoft/conf.txt
+grep contract /etc/asterisk/cel_prostiezvonki.conf >> /backup_vedisoft/conf.txt
+grep channel_type /etc/asterisk/cel_prostiezvonki.conf >> /backup_vedisoft/conf.txt
+}
 astver=$(asterisk -V | grep -woE [0-9]+\.)
 echo "make PZ new backup"
 {
@@ -20,6 +27,8 @@ mv /backup_vedisoft /backup_vedisoft_old
 mkbackup
 fi
 } &> /dev/null
+echo "Getting some settings from conf"
+cinfo
 echo "Downloading new vedisoft version"
 {
 mkdir -p /root/srcPZ/
