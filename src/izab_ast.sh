8efb1e71a786bd605c5595acbd6b4bd69584d8ff
rstayalive <rstayalive@users.noreply.github.com>
2020-07-28 16:35:51 +0300

    Update izab_ast.sh

--- src/izab_ast.sh
+++ src/izab_ast.sh
@@ -4,7 +4,7 @@ eip=`fwconsole extip`
 cp /root/setupmenu/src/asterisk_stat.sh /etc/zabbix/
 replace "# Timeout=3" "Timeout=8" -- /etc/zabbix/zabbix_agentd.conf
 replace "# SourceIP=" "SourceIP=$eip" -- /etc/zabbix/zabbix_agentd.conf
-echo "UserParameter		= apache_status,/etc/zabbix/apache_stat.sh" >> /etx/zabbix/
+echo "UserParameter		= apache_status,/etc/zabbix/apache_stat.sh" >> /etc/zabbix/zabbix_agentd.conf
 chmod 2750 /etc/zabbix; chgrp -R zabbix /etc/zabbix
 chmod 640 /etc/zabbix/zabbix_agentd.conf
 chmod 750 /etc/zabbix/asterisk_stat.sh
