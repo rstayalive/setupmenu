6fdc6b407cf525932ae7dcec37d2ca7cb08ffb81
rstayalive <rstayalive@gmail.com>
2022-04-15 10:56:18 +0300

    changed restart command

--- src/certificate_install.sh
+++ src/certificate_install.sh
@@ -29,7 +29,7 @@ mv privkey1.pem /etc/asterisk/oldkeys
 ln -s /etc/asterisk/keys/$(hostname)/cert.pem /etc/asterisk/newsert.pem
 ln -s /etc/asterisk/keys/$(hostname)/private.pem /etc/asterisk/privkey1.pem
 chown -R asterisk:asterisk /etc/asterisk
-/etc/init.d/asterisk restart
+service asterisk restart
 echo -e "Дата начала действия сертификата$startdate
 Дата окончания действия сертификата$expiredate"
 fi
