c01c562d0c8e88805019f18c594d9f1b0db4a7a8
rstayalive <rstayalive@gmail.com>
2023-04-14 14:32:13 +0300

    about script

--- src/trunkalert.sh
+++ src/trunkalert.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #setup trunk failure script
+#Edit trunk - monitor trunk failures and add trunkalert.agi,sipnet(or some turnk name)
 
 wget -O /var/lib/asterisk/agi-bin/trunkalert.agi https://raw.githubusercontent.com/sorvani/freepbx-helper-scripts/master/Monitor_Trunk_Failure/trunkalert.agi
 
