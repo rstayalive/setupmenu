1f98c91b528e9760358d7094278654e33d64633a
rstayalive <rstayalive@gmail.com>
2024-07-10 15:06:42 +0300

    update asterisk info gather

--- src/info.sh
+++ src/info.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#asterisk info gather script
 end()
 {
 echo -e "файл с иформацией лежит $out, нажмите любую кнопку для продолжения"
@@ -10,6 +11,13 @@ then arc=64
 else arc=86
 fi
 astver=$(asterisk -V | grep -woE [0-9]+\.)
+ami=$(asterisk -rx 'manager show settings')
+cel=$(asterisk -rx 'cel show status')
+celcheck=$(mysql asteriskcdrdb --execute= select id, eventtype, eventtime, cid_num, exten, uniqueid, linkedid, channame from cel order by id desc limit 10;)
+cdrcheck=$(mysql asteriskcdrdb --execute= select recordingfile from cdr order by calldate desc limit 10;)
+phpver=$(php -v)
+phpjson=$(php -r)
+phpcurl=$(php -r 'echo curl_version()["version"];')
 out="/tmp/outputinfo.txt"
 uname=`uname -r`
 red=`cat /etc/redhat-release`
@@ -20,6 +28,9 @@ freepbx=`rpm -qa | grep freepbx`
 cpu=`cat /proc/cpuinfo`
 mem=`free -m`
 disk=`df -kh .`
+#clear old out info
+echo 'Remove old out info file, if any...'
+rm -f "${out}"
 #gathering system and asterisk info
 echo "linux $uname" >> $out
 if ! [ -f /etc/redhat-release ];
@@ -36,4 +47,11 @@ echo "$ip" >> $out
 echo "$cpu" >> $out
 echo "$mem" >> $out
 echo "$disk" >> $out
+echo "ami $ami" >> $out
+echo "cel status $cel" >> $out
+echo "cel check $celcheck" >> $out
+echo "cdr check $cdrcheck" >> $out
+echo "php $phpver" >> $out
+echo "php json $phpjson" >> $out
+echo "php curl $phpcurl" >> $out
 end
