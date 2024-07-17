f34385f359e74314ac967c94548e5a2ee0c8135e
rstayalive <rstayalive@gmail.com>
2024-07-17 13:07:24 +0300

    FreePBX PJSIP cron trunk monitoring and alert to telegram

--- src/trk_pjsip_monitoring.sh
+++ src/trk_pjsip_monitoring.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 #FreePBX PJSIP cron trunk monitoring
 #please add cron job example: - crontab -e */5 * * * * /path to script
-
+#create a telegram bot or get bot TOKEN
+#get your chat ID - Search for the userinfobot on Telegram and start a chat. Send /start and note down the chat ID that you receive in the response. This is where the bot will send messages.
 # Telegram API token and chat ID
 TELEGRAM_TOKEN=2060869224:AAH4k2ug5dAbBgEAlKjcPliAVAQoIdKzZIA
 CHAT_ID=232107968
+#get freepbx hostname for message
 hname=`hostname`
+#get external IP for message
 extip=`fwconsole extip`
 
 # FreePBX command to check trunk status
