4ac6b6a04f6b032d222fa546234bcc1347edad8a
rstayalive <rstayalive@gmail.com>
2024-07-19 11:39:36 +0300

    cdrfix beta update

--- src/cdrfix.sh
+++ src/cdrfix.sh
@@ -1,23 +1,45 @@
 #!/bin/bash
 #cdr codepage fix
+# Ensure the script is run as root
+if [ "$(id -u)" -ne 0 ]; then
+    echo "This script must be run as root. Please use sudo."
+    exit 1
+fi
 #backup db
-mysqldump -u root --all-databases | gzip > /root/database.sql.gz
+backup_file="/root/freepbx_$(date +'%Y%m%d_%H%M%S').sql.gz"
+mysqldump --extended-insert --all-databases --add-drop-database --disable-keys --flush-privileges --quick --routines --triggers | gzip > "$backup_file"
 #installing mysql odbc connector
-odbc=`rpm -qa | grep mysql-connector-odbc`
-rpm -e --nodeps $odbc
+#odbc=`rpm -qa | grep mysql-connector-odbc`
+#rpm -e --nodeps $odbc
+odbc_installed=$(rpm -qa | grep -q mysql-connector-odbc && echo "yes" || echo "no")
+
+if [ "$odbc_installed" = "yes" ]; then
+    rpm -e --nodeps $(rpm -qa | grep mysql-connector-odbc)
+fi
 yum install mariadb-connector-odbc -y
-#check config for [mariadb] configuration
-if grep -F -x '[MariaDB]' /etc/odbcinst.ini;
-then
-echo "already configured"
+# Configure MariaDB ODBC connector
+echo "Configuring MariaDB ODBC connector..."
+if grep -q '^\[MariaDB\]' /etc/odbcinst.ini; then
+    echo "MariaDB ODBC connector already configured."
 else
-#configuring mariadb odbc connector
-echo "[MariaDB]
+    echo "[MariaDB]
 Description=ODBC for MariaDB
 Driver=/usr/lib64/libmaodbc.so
 Setup=/usr/lib64/libodbcmyS.so
 UsageCount=2" >> /etc/odbcinst.ini
-fi
+#check config for [mariadb] configuration
+#if grep -F -x '[MariaDB]' /etc/odbcinst.ini;
+#then
+#echo "already configured"
+#else
+#configuring mariadb odbc connector
+#echo "[MariaDB]
+#Description=ODBC for MariaDB
+#Driver=/usr/lib64/libmaodbc.so
+#Setup=/usr/lib64/libodbcmyS.so
+#UsageCount=2" >> /etc/odbcinst.ini
+#fi
+# Restart FreePBX services
 fwconsole restart
 clear
 echo "all done!"
