e3b2675fe9216c572f88cee09f2178a4dfc3d791
rstayalive <rstayalive@gmail.com>
2024-07-19 17:30:20 +0300

    info gather update

--- src/info.sh
+++ src/info.sh
@@ -5,11 +5,14 @@ end()
 echo -e "файл с иформацией лежит $out, нажмите любую кнопку для продолжения"
 read -s -n 1
 }
+# Determine architecture
 arc=`arch`
-if [ "$arc" == "x86_64" ];
-then arc=64
-else arc=86
+if [ "$arc" == "x86_64" ];then
+ arc=64
+else
+ arc=86
 fi
+# Gather system information
 astver=$(asterisk -V | grep -woE [0-9]+\.)
 ami=$(asterisk -rx 'manager show settings')
 cel=$(asterisk -rx 'cel show status')
@@ -18,40 +21,44 @@ cdrcheck=$(mysql asteriskcdrdb --execute="SELECT recordingfile from cdr order by
 phpver=$(php -v)
 phpjson=$(php -r 'var_dump(function_exists("json_decode"));')
 phpcurl=$(php -r 'echo curl_version()["version"];')
+# Output file
 out="/tmp/outputinfo.txt"
-uname=`uname -r`
-red=`cat /etc/redhat-release`
-deb=`cat /etc/debian_version`
-pbxfirm=`cat /etc/schmooze/pbx-version`
-ip=`fwconsole extip`
-freepbx=`rpm -qa | grep freepbx`
-cpu=`cat /proc/cpuinfo`
-mem=`free -m`
-disk=`df -kh .`
+# Gather additional system information
+uname=$(uname -r)
+if [ -f /etc/redhat-release ]; then
+    red=$(cat /etc/redhat-release)
+    os_version="$red"
+else
+    deb=$(cat /etc/debian_version)
+    os_version="$deb"
+fi
+pbxfirm=$(cat /etc/schmooze/pbx-version 2>/dev/null)
+ip=$(fwconsole extip 2>/dev/null)
+freepbx=$(rpm -qa | grep freepbx)
+cpu=$(cat /proc/cpuinfo)
+mem=$(free -m)
+disk=$(df -kh)
 #clear old out info
 echo 'Remove old out info file, if any...'
 rm -f "${out}"
 #gathering system and asterisk info
-echo "linux $uname" >> $out
-if ! [ -f /etc/redhat-release ];
-then
-echo "$deb" >> $out
-else
-echo "$red" >> $out
-fi
-echo "freepbx rpm ver $freepbx" >> $out
-echo "freepbx firmware ver $pbxfirm" >> $out
-echo "asterisk $astver" >> $out
-echo "arch x$arc" >> $out
-echo "EXT IP $ip" >> $out
-echo "$cpu" >> $out
-echo "$mem" >> $out
-echo "$disk" >> $out
-echo "AMI check: $ami" >> $out
-echo "CEL status check: $cel" >> $out
-echo "CEL check $celcheck" >> $out
-echo "CDR check $cdrcheck" >> $out
-echo "$phpver" >> $out
-echo "PHP JSON check $phpjson" >> $out
-echo "PHP CURL check $phpcurl" >> $out
+{
+    echo "Linux Kernel Version: $uname"
+    echo "OS Version: $os_version"
+    echo "Architecture: x$arc"
+    echo "External IP: $ip"
+    echo "CPU Info: $cpu"
+    echo "Memory Info: $mem"
+    echo "Disk Usage: $disk"
+    echo "FreePBX RPM Version: $freepbx"
+    echo "FreePBX Firmware Version: $pbxfirm"
+    echo "Asterisk Version: $astver"
+    echo "AMI Settings: $ami"
+    echo "CEL Status: $cel"
+    echo "CEL Check: $celcheck"
+    echo "CDR Check: $cdrcheck"
+    echo "PHP Version: $phpver"
+    echo "PHP JSON Check: $phpjson"
+    echo "PHP CURL Version: $phpcurl"
+} > "$out"
 end
