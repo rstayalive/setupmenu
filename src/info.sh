ae68a6b207950419faef34a4d5a7d2a25db5ca01
rstayalive <rstayalive@gmail.com>
2024-12-25 02:08:11 +0300

    new info script

--- src/info.sh
+++ src/info.sh
@@ -1,87 +1,114 @@
 #!/bin/bash
-#asterisk info gather script
-# Determine architecture
-arc=`arch`
-if [ "$arc" == "x86_64" ];then
- arc=64
-else
- arc=86
+
+# Файл для вывода отчета
+REPORT_FILE="/tmp/asterisk_report.txt"
+
+# Функция для записи в отчет
+write_to_report() {
+  echo -e "$1" >> $REPORT_FILE
+}
+
+# Путь к конфигурационному файлу
+CONFIG_FILE="/etc/freepbx.conf"
+
+# Проверка наличия конфигурационного файла
+if [[ ! -f $CONFIG_FILE ]]; then
+  echo "Error: Configuration file $CONFIG_FILE not found!" >&2
+  exit 1
 fi
-# Gather system information
-astver=$(asterisk -V | grep -woE [0-9]+\.)
-ami=$(asterisk -rx 'manager show settings')
-cel=$(asterisk -rx 'cel show status')
-odbc=$(asterisk -rx 'odbc show')
-celcheck=$(mysql asteriskcdrdb --execute="SELECT id, eventtype, eventtime, cid_num, exten, uniqueid, linkedid, channame FROM cel ORDER BY id DESC LIMIT 10;" 2>/dev/null)
-cdrcheck=$(mysql asteriskcdrdb --execute="SELECT recordingfile FROM cdr ORDER BY calldate DESC LIMIT 10;" 2>/dev/null)
-phpver=$(php -v)
-phpjson=$(php -r 'var_dump(function_exists("json_decode"));')
-phpcurl=$(php -r 'echo curl_version()["version"];')
-# Output file
-out="/tmp/outputinfo.txt"
-# Gather additional system information
-uname=$(uname -r)
-if [ -f /etc/redhat-release ]; then
-    os_version=$(cat /etc/redhat-release)
-else
-    os_version=$(cat /etc/debian_version)
-fi
-pbxfirm=$(cat /etc/schmooze/pbx-version 2>/dev/null)
-ip=$(fwconsole extip 2>/dev/null)
-freepbx=$(rpm -qa | grep freepbx)
-cpu=$(cat /proc/cpuinfo)
-mem=$(free -h)
-disk=$(df -kh)
-#clear old out info
-rm -f "${out}"
-#gathering system and asterisk info
-{
-    echo "──────────────────────────────────────────────────────"
-    echo "Linux Kernel Version: $uname"
-    echo "──────────────────────────────────────────────────────"
-    echo "OS Version: $os_version"
-    echo "──────────────────────────────────────────────────────"
-    echo "Architecture: x$arc"
-    echo "──────────────────────────────────────────────────────"
-    echo "External IP: $ip"
-    echo "──────────────────────────────────────────────────────"
-    echo "CPU Info:"
-    echo "$cpu"
-    echo "──────────────────────────────────────────────────────"
-    echo "Memory Info:"
-    echo "$mem"
-    echo "──────────────────────────────────────────────────────"
-    echo "Disk Usage:"
-    echo "$disk"
-    echo "──────────────────────────────────────────────────────"
-    echo "FreePBX RPM Version: $freepbx"
-    echo "──────────────────────────────────────────────────────"
-    echo "FreePBX Firmware Version: $pbxfirm"
-    echo "──────────────────────────────────────────────────────"
-    echo "Asterisk Version: $astver"
-    echo "──────────────────────────────────────────────────────"
-    echo "AMI Settings:"
-    echo "$ami"
-    echo "──────────────────────────────────────────────────────"
-    echo "CEL Status:"
-    echo "$cel"
-    echo "──────────────────────────────────────────────────────"
-    echo "ODBC Status:"
-    echo "$odbc"
-    echo "──────────────────────────────────────────────────────"
-    echo "CEL Check:"
-    echo "$celcheck"
-    echo "──────────────────────────────────────────────────────"
-    echo "CDR Check:"
-    echo "$cdrcheck"
-    echo "──────────────────────────────────────────────────────"
-    echo "PHP Version: $phpver"
-    echo "──────────────────────────────────────────────────────"
-    echo "PHP JSON Check:"
-    echo "$phpjson"
-    echo "──────────────────────────────────────────────────────"
-    echo "PHP CURL Version:"
-    echo "$phpcurl"
-    echo "──────────────────────────────────────────────────────"
-} > "$out"
-echo -e "Файл отчета лежит по пути: $out"
+
+# Извлечение параметров из файла
+DB_USER=$(grep -oP '\$amp_conf\["AMPDBUSER"\]\s*=\s*"\K[^"]+' $CONFIG_FILE)
+DB_PASS=$(grep -oP '\$amp_conf\["AMPDBPASS"\]\s*=\s*"\K[^"]+' $CONFIG_FILE)
+DB_HOST=$(grep -oP '\$amp_conf\["AMPDBHOST"\]\s*=\s*"\K[^"]+' $CONFIG_FILE)
+DB_PORT=$(grep -oP '\$amp_conf\["AMPDBPORT"\]\s*=\s*"\K[^"]+' $CONFIG_FILE)
+DB_NAME=$(grep -oP '\$amp_conf\["AMPDBNAME"\]\s*=\s*"\K[^"]+' $CONFIG_FILE)
+
+# Очистка файла отчета
+> $REPORT_FILE
+
+# Информация о версии скрипта
+write_to_report "\n===== Asterisk System Report =====\n"
+write_to_report "Report Date: $(date)"
+write_to_report "\n==================================\n"
+
+# Информация о системе
+write_to_report "\nSystem Information:\n"
+write_to_report "Kernel Version: $(uname -r)"
+write_to_report "OS Version: $(grep PRETTY_NAME /etc/os-release | cut -d '"' -f2)"
+write_to_report "Architecture: $(uname -m)"
+write_to_report "CPU Info: $(lscpu | grep "Model name" | awk -F ':' '{print $2}' | xargs)"
+write_to_report "Memory Info: $(free -h | grep "Mem:" | awk '{print $2" total, "$3" used, "$4" free"}')"
+write_to_report "Disk Usage:\n$(df -kh)"
+write_to_report "\nPublic IP:\n $(fwconsole extip 2>/dev/null || echo "Unavailable")"
+
+# Версия FreePBX и Asterisk
+write_to_report "\nFreePBX and Asterisk Versions:\n"
+write_to_report "FreePBX Version: $(cat /etc/sangoma/pbx-version 2>/dev/null || echo "Unavailable")"
+write_to_report "Asterisk Version: $(asterisk -V 2>/dev/null | grep -woE '[0-9]+\.[0-9]+' || echo "Unavailable")"
+
+# Настройки AMI
+write_to_report "\nAsterisk AMI Settings:\n"
+write_to_report "$(asterisk -rx 'manager show settings' 2>/dev/null || echo "AMI Settings Unavailable")"
+
+# Статус CEL
+write_to_report "\nCEL Status:\n"
+write_to_report "$(asterisk -rx 'cel show status' 2>/dev/null || echo "CEL Status Unavailable")"
+
+# Статус ODBC
+write_to_report "\nODBC Status:\n"
+write_to_report "$(asterisk -rx 'odbc show' 2>/dev/null || echo "ODBC Status Unavailable")"
+
+# Информация о PHP,JSON,CURL
+write_to_report "\n===== PHP check =====\n"
+# Версия PHP
+write_to_report "PHP version:"
+write_to_report "$(php -v)"
+
+# Работа PHPJSON
+write_to_report "PHPJSON:"
+write_to_report "$(php -r 'var_dump(function_exists("json_decode"));')"
+
+# Работа PHPCURL
+write_to_report "PHPCURL:"
+write_to_report "$(php -r 'echo curl_version()["version"];')"
+write_to_report "\n==================================\n"
+
+# Последние записи из баз данных
+write_to_report "\nLast 10 CEL Records:\n"
+mysql -u$DB_USER -p$DB_PASS asteriskcdrdb -e "SELECT id, eventtype, eventtime, cid_num, exten, uniqueid, linkedid, channame FROM cel ORDER BY id DESC LIMIT 10;" 2>/dev/null | column -t >> $REPORT_FILE
+
+write_to_report "\nLast 10 CDR Records:\n"
+mysql -u$DB_USER -p$DB_PASS asteriskcdrdb -e "SELECT recordingfile FROM cdr ORDER BY calldate DESC LIMIT 10;" 2>/dev/null | column -t >> $REPORT_FILE
+
+# Используемые extensions
+write_to_report "\nSIP Extensions:\n"
+rasterisk -rx "sip show peers" 2>/dev/null | awk '/^[0-9]+\/[0-9]+/ {split($1, a, "/"); print a[1]}' | awk 'length($0) < 5' >> $REPORT_FILE
+
+write_to_report "\nPJSIP Extensions:\n"
+rasterisk -rx "pjsip list endpoints" 2>/dev/null | awk '/Endpoint:/ {print $2}' | sed 's#/.*##' | grep -E '^[0-9]{1,4}$' >> $REPORT_FILE
+
+# Используемые ringgroups
+write_to_report "\nRing Groups:\n"
+mysql -u$DB_USER -p$DB_PASS -h$DB_HOST -P$DB_PORT $DB_NAME -e "SELECT grpnum, strategy, grptime, grplist, postdest, recording FROM ringgroups;" 2>/dev/null | column -t >> $REPORT_FILE
+
+# Используемые queues
+write_to_report "\nQueues Config:\n"
+mysql -u$DB_USER -p$DB_PASS asterisk -e "SELECT extension, descr, dest FROM queues_config;" 2>/dev/null | column -t >> $REPORT_FILE
+
+write_to_report "\nQueues Details:\n"
+mysql -u$DB_USER -p$DB_PASS asterisk -e "SELECT id, keyword, data FROM queues_details;" 2>/dev/null | column -t >> $REPORT_FILE
+
+# Используемые IVR
+write_to_report "\nIVR Details:\n"
+mysql -u$DB_USER -p$DB_PASS asterisk -e "SELECT id, name, invalid_destination, timeout_time, timeout_destination FROM ivr_details;" 2>/dev/null | column -t >> $REPORT_FILE
+
+# Используемые trunks
+write_to_report "\nSIP Trunks:\n"
+write_to_report "$(asterisk -rx "sip show registry" 2>/dev/null || echo "Unavailable")"
+
+write_to_report "\nPJSIP Trunks:\n"
+write_to_report "$(asterisk -rx "pjsip list registrations" 2>/dev/null || echo "Unavailable")"
+
+# Печать пути к отчету
+echo "Отчет сформирован и сохранен в | Report generated and saved to $REPORT_FILE"
--- src/old_info.sh
+++ src/old_info.sh
@@ -1,29 +1,5 @@
 #!/bin/bash
-# Define the configuration file
-config_file="/etc/freepbx.conf"
-# Output file
-report="/tmp/asterisk_report.txt"
-
-# Function to extract the value of a given key from the PHP configuration file
-extract_value() {
-    local key=$1
-    awk -v key="$key" '
-    BEGIN { FS=" *= *" }
-    $1 ~ "\\$amp_conf\\[\\s*\"" key "\\\"" { gsub(/[";]/, "", $2); print $2 }
-    ' "$config_file"
-}
-
-# Extract db credentials
-username=$(extract_value "AMPDBUSER")
-password=$(extract_value "AMPDBPASS")
-host=$(extract_value "AMPDBHOST")
-databasename=$(extract_value "AMPDBNAME")
-
-# MySQL credentials
-DB_USER="$username"
-DB_PASS="$password"
-DB_NAME="$databasename"
-
+#asterisk info gather script
 # Determine architecture
 arc=`arch`
 if [ "$arc" == "x86_64" ];then
@@ -31,7 +7,18 @@ if [ "$arc" == "x86_64" ];then
 else
  arc=86
 fi
-
+# Gather system information
+astver=$(asterisk -V | grep -woE [0-9]+\.)
+ami=$(asterisk -rx 'manager show settings')
+cel=$(asterisk -rx 'cel show status')
+odbc=$(asterisk -rx 'odbc show')
+celcheck=$(mysql asteriskcdrdb --execute="SELECT id, eventtype, eventtime, cid_num, exten, uniqueid, linkedid, channame FROM cel ORDER BY id DESC LIMIT 10;" 2>/dev/null)
+cdrcheck=$(mysql asteriskcdrdb --execute="SELECT recordingfile FROM cdr ORDER BY calldate DESC LIMIT 10;" 2>/dev/null)
+phpver=$(php -v)
+phpjson=$(php -r 'var_dump(function_exists("json_decode"));')
+phpcurl=$(php -r 'echo curl_version()["version"];')
+# Output file
+out="/tmp/outputinfo.txt"
 # Gather additional system information
 uname=$(uname -r)
 if [ -f /etc/redhat-release ]; then
@@ -39,41 +26,15 @@ if [ -f /etc/redhat-release ]; then
 else
     os_version=$(cat /etc/debian_version)
 fi
-
-# Gather information
-hname=$(hostname)
-astver=$(asterisk -V | grep -woE [0-9]+\.)
-ami=$(asterisk -rx 'manager show settings')
-cel=$(asterisk -rx 'cel show status')
-odbc=$(asterisk -rx 'odbc show')
-celcheck=$(mysql -u $DB_USER -p$DB_PASS asteriskcdrdb --execute="SELECT id, eventtype, eventtime, cid_num, exten, uniqueid, linkedid, channame FROM cel ORDER BY id DESC LIMIT 10;" 2>/dev/null)
-cdrcheck=$(mysql -u $DB_USER -p$DB_PASS asteriskcdrdb --execute="SELECT recordingfile FROM cdr ORDER BY calldate DESC LIMIT 10;" 2>/dev/null)
-phpver=$(php -v)
-phpjson=$(php -r 'var_dump(function_exists("json_decode"));')
-phpcurl=$(php -r 'echo curl_version()["version"];')
 pbxfirm=$(cat /etc/schmooze/pbx-version 2>/dev/null)
 ip=$(fwconsole extip 2>/dev/null)
 freepbx=$(rpm -qa | grep freepbx)
 cpu=$(cat /proc/cpuinfo)
 mem=$(free -h)
 disk=$(df -kh)
-# Function to gather extensions
-sipext=$(rasterisk -rx "sip show peers" | awk '/^[0-9]+\/[0-9]+/ {split($1, a, "/"); print a[1]}' | awk 'length($0) < 5')
-pjsipext=$(rasterisk -rx "pjsip list endpoints" | awk '/Endpoint:/{print $2}' | sed 's#/.*##' | grep -E '^[0-9]{1,4}$')
-# Function to gather trunks
-siptrunk=$(asterisk -rx "sip show registry")
-pjsiptrunk=$(asterisk -rx "pjsip list registrations")
-# Function to gather ring groups
-ringgroups=$(mysql -u $DB_USER -p$DB_PASS $DB_NAME -e "SELECT grpnum, strategy, grppre, grplist, postdest FROM ringgroups")
-# Function to gather queues
-queues=$(mysql -u $DB_USER -p$DB_PASS $DB_NAME -e "SELECT extension, descr, dest FROM queues_config")
-# Function to gather IVR
-ivrmenu=$(mysql -u $DB_USER -p$DB_PASS $DB_NAME -e "SELECT id, name, directdial, timeout_time, timeout_destination, invalid_destination FROM ivr_details")
-
-# Clear the output file
-> $report
-
-# Making report
+#clear old out info
+rm -f "${out}"
+#gathering system and asterisk info
 {
     echo "──────────────────────────────────────────────────────"
     echo "Linux Kernel Version: $uname"
@@ -105,6 +66,9 @@ ivrmenu=$(mysql -u $DB_USER -p$DB_PASS $DB_NAME -e "SELECT id, name, directdial,
     echo "CEL Status:"
     echo "$cel"
     echo "──────────────────────────────────────────────────────"
+    echo "ODBC Status:"
+    echo "$odbc"
+    echo "──────────────────────────────────────────────────────"
     echo "CEL Check:"
     echo "$celcheck"
     echo "──────────────────────────────────────────────────────"
@@ -119,26 +83,5 @@ ivrmenu=$(mysql -u $DB_USER -p$DB_PASS $DB_NAME -e "SELECT id, name, directdial,
     echo "PHP CURL Version:"
     echo "$phpcurl"
     echo "──────────────────────────────────────────────────────"
-    echo "SIP extensions"
-    echo "$sipext"
-    echo "──────────────────────────────────────────────────────"
-    echo "PJSIP extensions"
-    echo "$pjsipext"
-    echo "──────────────────────────────────────────────────────"
-    echo "Ring Groups"
-    echo "$ringgroups"
-    echo "──────────────────────────────────────────────────────"
-    echo "Queues"
-    echo "$queues"
-    echo "──────────────────────────────────────────────────────"
-    echo "IVR"
-    echo "$ivrmenu"
-    echo "──────────────────────────────────────────────────────"
-    echo "SIP Trunks"
-    echo "$siptrunk"
-    echo "──────────────────────────────────────────────────────"
-    echo "PJSIP Trunks"
-    echo "$pjsiptrunk"
-    echo "──────────────────────────────────────────────────────"
-} > "$report"
-echo -e "Файл отчета лежит по пути: $report"
+} > "$out"
+echo -e "Файл отчета лежит по пути: $out"
