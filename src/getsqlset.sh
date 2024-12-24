b3df59d0951bfe8c1cca11b19936b19dd84d9016
rstayalive <rstayalive@gmail.com>
2024-12-25 02:13:42 +0300

    some fixes

--- src/getsqlset.sh
+++ src/getsqlset.sh
@@ -6,12 +6,10 @@ CONFIG_FILE="/etc/freepbx.conf"
 # Извлечение параметров из файла
 DB_USER=$(grep -oP '\$amp_conf\["AMPDBUSER"\]\s*=\s*"\K[^"]+' $CONFIG_FILE)
 DB_PASS=$(grep -oP '\$amp_conf\["AMPDBPASS"\]\s*=\s*"\K[^"]+' $CONFIG_FILE)
-DB_HOST=$(grep -oP '\$amp_conf\["AMPDBHOST"\]\s*=\s*"\K[^"]+' $CONFIG_FILE)
-DB_PORT=$(grep -oP '\$amp_conf\["AMPDBPORT"\]\s*=\s*"\K[^"]+' $CONFIG_FILE)
 DB_NAME=$(grep -oP '\$amp_conf\["AMPDBNAME"\]\s*=\s*"\K[^"]+' $CONFIG_FILE)
 
 # Проверка подключения к базе данных
-if mysql -u"$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" -e "USE $DB_NAME;" >/dev/null 2>&1; then
+if mysql -u"$DB_USER" -p"$DB_PASS" -e "USE asteriskcdrdb;" >/dev/null 2>&1; then
     echo "Подключение к базе данных $DB_NAME успешно!"
 else
     echo "Ошибка подключения к базе данных $DB_NAME."
